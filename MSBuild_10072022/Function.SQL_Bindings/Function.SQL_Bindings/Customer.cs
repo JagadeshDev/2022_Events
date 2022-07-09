6bba2d957bbfe7ab677d91d17d5e1cec29b29f09
Jagadesh <jagadesh.julapalli@gmail.com>
2022-07-09 21:48:05 +0530

    Added POCO classes from the DB Tables

---
+++ MSBuild_10072022/Function.SQL_Bindings/Function.SQL_Bindings/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Company.Function
+{
+    public class Customer
+    {
+        [JsonPropertyName("customerId")]
+        public string CustomerId { get; set; }
+
+        [JsonPropertyName("customerName")]
+        public string CustomerName { get; set; }
+
+        [JsonPropertyName("customerPhone")]
+        public string CustomerPhone { get; set; }
+    }
+
+}
---
+++ MSBuild_10072022/Function.SQL_Bindings/Function.SQL_Bindings/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Company.Function
+{
+    public class Order
+    {
+        [JsonPropertyName("orderId")]
+        public string? OrderId { get; set; }
+
+        [JsonPropertyName("item")]
+        public string? Item { get; set; }
+
+        [JsonPropertyName("price")]
+        public double Price { get; set; }
+    }
+
+}
